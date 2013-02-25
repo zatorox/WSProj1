a5a10a58da3b60762aca38ddea49a619014cf2a0
Takostia <jsbrodersen@gmail.com>
2013-02-25 16:23:38 -0500

    Commit of general parser

--- WSProj1/Browser.py
+++ WSProj1/Browser.py
@@ -11,8 +11,8 @@
 import random
 import socket
 import urllib
-import urllib2
-import httplib
+import urllib.request
+import http.client
 
 BROWSERS = (
     # Top most popular browsers in my access.log on 2009.02.12
@@ -43,7 +43,7 @@ class BrowserError(Exception):
         self.url = url
         self.error = error
 
-class PoolHTTPConnection(httplib.HTTPConnection):
+class PoolHTTPConnection(http.client.HTTPConnection):
     def connect(self):
         """Connect to the host and port specified in __init__."""
         msg = "getaddrinfo returns an empty list"
@@ -53,21 +53,21 @@ class PoolHTTPConnection(httplib.HTTPConnection):
             try:
                 self.sock = socket.socket(af, socktype, proto)
                 if self.debuglevel > 0:
-                    print "connect: (%s, %s)" % (self.host, self.port)
+                    print ('connect: (%s, %s)') % (self.host, self.port)
                 self.sock.settimeout(TIMEOUT)
                 self.sock.connect(sa)
-            except socket.error, msg:
+            except socket.error(msg):
                 if self.debuglevel > 0:
-                    print 'connect fail:', (self.host, self.port)
+                    print('connect fail:'), (self.host, self.port)
                 if self.sock:
                     self.sock.close()
                 self.sock = None
                 continue
             break
         if not self.sock:
-            raise socket.error, msg
+            raise socket.error(msg)
 
-class PoolHTTPHandler(urllib2.HTTPHandler):
+class PoolHTTPHandler(urllib.request.HTTPHandler):
     def http_open(self, req):
         return self.do_open(PoolHTTPConnection, req)
 
@@ -82,17 +82,17 @@ class Browser(object):
 
     def get_page(self, url, data=None):
         handlers = [PoolHTTPHandler]
-        opener = urllib2.build_opener(*handlers)
+        opener = urllib.request.build_opener(*handlers)
         if data: data = urllib.urlencode(data)
-        request = urllib2.Request(url, data, self.headers)
+        request = urllib.request.Request(url, data, self.headers)
         try:
             response = opener.open(request)
             return response.read()
-        except (urllib2.HTTPError, urllib2.URLError), e:
-            raise BrowserError(url, str(e))
-        except (socket.error, socket.sslerror), msg:
+        except (urllib.request.HTTPError, urllib.request.URLError):
+            raise BrowserError(url, str)
+        except (socket.error, socket.sslerror) (msg):
             raise BrowserError(url, msg)
-        except socket.timeout, e:
+        except socket.timeout (e):
             raise BrowserError(url, "timeout")
         except KeyboardInterrupt:
             raise
---
+++ WSProj1/GeneralParser.py
@@ -0,0 +1,38 @@
+import urllib
+import json
+
+from Browser import Browser, BrowserError
+
+class SearchError(Exception):
+    """
+    Base class for search exceptions.
+    """
+    pass
+
+class FarooSearch(object):
+
+    SEARCH_URL = "http://www.faroo.com/api?q=%(query)s&start=%(start)s&length=%(length)s&l=en&src=web&f=json"
+
+    def __init__(self, query, start, length):
+        self.query = query
+        self.start = start
+        self.length = length
+        self.browser = Browser()
+
+    def _get_results(self):
+
+        url = FarooSearch.SEARCH_URL
+        
+        actual_url = url % {'query': self.query,
+                         'start': self.start,
+                         'length': self.length}
+
+        try:
+            page = self.browser.get_page(actual_url)
+        except BrowserError:
+            raise SearchError ("Failed getting %s: %s") % (e.url, e.error)
+
+        print("'''" + page.decode("utf-8") + "'''")
+        return "'''" + page.decode("utf-8") + "'''"
+        
+
