7d7ac030e33a7810aaa16b290d62e96d111de7be
Takostia <jsbrodersen@gmail.com>
2013-02-26 15:13:26 -0500

    Small changes in johanTest

--- WSProj1/johanTest.py
+++ WSProj1/johanTest.py
@@ -2,9 +2,8 @@ from GeneralParser import FarooSearch, SearchError
 
 try:
     fs = FarooSearch("quidditch", "1", "5")
-    #gs.results_per_page = 50
     results = fs._get_results()
 except SearchError:
-    print ("Search failed: %s") % e
+    print ("Search failed: %s")
 
 
